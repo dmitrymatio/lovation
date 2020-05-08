7be19fcfe156a49ad1c1683692a0d96f64206fd0
Dmitry Matio <dmitrymatio@gmail.com>
2020-05-07 23:49:39 -0700

    switched strapi builds

--- backend/config/functions/cron.js
+++ backend/config/functions/cron.js
@@ -5,7 +5,7 @@
  * to run scheduled jobs.
  *
  * The cron format consists of:
- * [SECOND (optional)] [MINUTE] [HOUR] [DAY OF MONTH] [MONTH OF YEAR] [DAY OF WEEK]
+ * [MINUTE] [HOUR] [DAY OF MONTH] [MONTH OF YEAR] [DAY OF WEEK] [YEAR (optional)]
  *
  * See more details here: https://strapi.io/documentation/3.0.0-beta.x/concepts/configurations.html#cron-tasks
  */
